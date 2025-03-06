cbad1fda166a0110ea9b413f62f6344f7c4d7a4d
nikhil yadav <nikhilyadav73642@gmail.com>
2025-03-06 14:52:15 +0530

    Update script.js
    do some basic changes

--- script.js
+++ script.js
@@ -5,42 +5,24 @@ const education = document.querySelector("#radio-basic");
 const home = document.querySelector("#radio-free");
 let dekstop = document.querySelector(".dekstop");
 
+document.addEventListener("DOMContentLoaded", () => {
+  const menuBtn = document.querySelector(".menu-btn");
+  const navLinks = document.querySelector(".nav-links");
 
-// //mobile layout 
-// document.querySelector(".menu-btn").addEventListener("click", () => {
-//   document.querySelector(".nav-links").classList.toggle("active");
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const cursor = document.querySelector('.cursor');
-
-document.addEventListener('mousemove', (e) => {
-    let x = e.clientX;
-    let y = e.clientY;
-
-    cursor.style.transform = `translate(${x - 25}px, ${y - 25}px)`;
+  menuBtn.addEventListener("click", () => {
+    navLinks.style.display =
+      navLinks.style.display === "flex" ? "none" : "flex";
+  });
 });
 
+const cursor = document.querySelector(".cursor");
 
+document.addEventListener("mousemove", (e) => {
+  let x = e.clientX;
+  let y = e.clientY;
 
-
-
+  cursor.style.transform = `translate(${x - 25}px, ${y - 25}px)`;
+});
 
 let op1 = document.querySelector("#op1");
 let op2 = document.querySelector("#op2");
@@ -52,29 +34,23 @@ let close1 = document.querySelector("#close1");
 let close2 = document.querySelector("#close2");
 let close3 = document.querySelector("#close3");
 
-
 home.addEventListener("click", () => {
-    op3.style.display = "none";
-    op4.style.display = "none";
-    op2.style.display = "none";
-    op1.style.display = "none";
+  op3.style.display = "none";
+  op4.style.display = "none";
+  op2.style.display = "none";
+  op1.style.display = "none";
 });
 
-
-
 education.addEventListener("click", () => {
-    op3.style.display = "none";
-    op4.style.display = "none";
-    op2.style.display = "none";
-    op1.style.display = "block";
+  op3.style.display = "none";
+  op4.style.display = "none";
+  op2.style.display = "none";
+  op1.style.display = "block";
 });
 
 close.addEventListener("click", () => {
-    op1.style.display = "none";
+  op1.style.display = "none";
 });
-
-
-
 
 about.addEventListener("click", () => {
   op1.style.display = "none";
@@ -86,28 +62,21 @@ close1.addEventListener("click", () => {
   op2.style.display = "none";
 });
 
-
-
-
 contact.addEventListener("click", () => {
-    op1.style.display = "none";
-    op4.style.display = "none";
-    op2.style.display = "none";
-    op3.style.display = "block";
+  op1.style.display = "none";
+  op4.style.display = "none";
+  op2.style.display = "none";
+  op3.style.display = "block";
 });
 close2.addEventListener("click", () => {
   op3.style.display = "none";
 });
 
-
-
-
-
 certificates.addEventListener("click", () => {
-    op1.style.display = "none";
-    op3.style.display = "none";
-    op2.style.display = "none";
-    op4.style.display = "block";
+  op1.style.display = "none";
+  op3.style.display = "none";
+  op2.style.display = "none";
+  op4.style.display = "block";
 });
 close3.addEventListener("click", () => {
   op4.style.display = "none";
